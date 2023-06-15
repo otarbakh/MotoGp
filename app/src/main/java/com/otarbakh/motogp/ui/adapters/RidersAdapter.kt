d93b8012104bc445942f9a83280aedd849256a01
otarbakh <otarbakh@gmail.com>
2023-06-15 15:34:56 +0400

    jorje martin, and zarco missing :))

--- app/src/main/java/com/otarbakh/motogp/ui/adapters/RidersAdapter.kt
+++ app/src/main/java/com/otarbakh/motogp/ui/adapters/RidersAdapter.kt
@@ -5,8 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.otarbakh.motogp.R
 import com.otarbakh.motogp.data.model.summary.Competitor
 import com.otarbakh.motogp.databinding.SingleRiderLayoutBinding
+import com.otarbakh.motogp.ui.riders.Riders
 
 class RidersAdapter :
     ListAdapter<Competitor, RidersAdapter.RidersViewHolder>(
@@ -38,6 +40,31 @@ class RidersAdapter :
                 tvTeam.text = model?.team!!.name
                 tvPoints.text = model?.result!!.points.toString()
                 tvPosition.text = model?.result!!.position.toString()
+
+
+                when (model!!.name) {
+
+                    Riders.BINDER.lastName ->binding.ivTeamIndicator.setImageResource(Riders.BINDER.imageResourceId)
+                    Riders.ESPARGARO_A.lastName -> binding.ivTeamIndicator.setImageResource(Riders.ESPARGARO_A.imageResourceId)
+                    Riders.MARQUEZ_A.lastName -> binding.ivTeamIndicator.setImageResource(Riders.MARQUEZ_A.imageResourceId)
+                    Riders.RINS.lastName -> binding.ivTeamIndicator.setImageResource(Riders.RINS.imageResourceId)
+                    Riders.FERNANDEZ_A.lastName -> binding.ivTeamIndicator.setImageResource(Riders.FERNANDEZ_A.imageResourceId)
+                    Riders.BASTIANINI.lastName -> binding.ivTeamIndicator.setImageResource(Riders.BASTIANINI.imageResourceId)
+                    Riders.QUARTARARO.lastName -> binding.ivTeamIndicator.setImageResource(Riders.QUARTARARO.imageResourceId)
+                    Riders.GIANNATONIO.lastName -> binding.ivTeamIndicator.setImageResource(Riders.GIANNATONIO.imageResourceId)
+                    Riders.BAGNAIA.lastName -> binding.ivTeamIndicator.setImageResource(Riders.BAGNAIA.imageResourceId)
+                    Riders.MORDBIDELLI.lastName -> binding.ivTeamIndicator.setImageResource(Riders.MORDBIDELLI.imageResourceId)
+                    Riders.MILLER.lastName -> binding.ivTeamIndicator.setImageResource(Riders.MILLER.imageResourceId)
+                    Riders.MIR.lastName -> binding.ivTeamIndicator.setImageResource(Riders.MIR.imageResourceId)
+                    Riders.MARINI.lastName -> binding.ivTeamIndicator.setImageResource(Riders.MARINI.imageResourceId)
+                    Riders.MARQUEZ_M.lastName -> binding.ivTeamIndicator.setImageResource(Riders.MARQUEZ_M.imageResourceId)
+                    Riders.BEZZECCHI.lastName -> binding.ivTeamIndicator.setImageResource(Riders.BEZZECCHI.imageResourceId)
+                    Riders.VINIALES.lastName -> binding.ivTeamIndicator.setImageResource(Riders.VINIALES.imageResourceId)
+                    Riders.OLIVEIRA.lastName -> binding.ivTeamIndicator.setImageResource(Riders.OLIVEIRA.imageResourceId)
+                    Riders.ESPARGARO_P.lastName -> binding.ivTeamIndicator.setImageResource(Riders.ESPARGARO_P.imageResourceId)
+                    Riders.FERNANDEZ_R.lastName -> binding.ivTeamIndicator.setImageResource(Riders.FERNANDEZ_R.imageResourceId)
+                    Riders.NAKAGAMI.lastName -> binding.ivTeamIndicator.setImageResource(Riders.NAKAGAMI.imageResourceId)
+                }
 
 
             }
---
+++ app/src/main/java/com/otarbakh/motogp/ui/riders/Riders.kt
@@ -0,0 +1,27 @@
+package com.otarbakh.motogp.ui.riders
+
+import com.otarbakh.motogp.R
+
+enum class Riders(val lastName: String, val imageResourceId: Int) {
+    ESPARGARO_A("Espargaro, Aleix", R.drawable.aleix_espargaro_aprilia_racing_1),
+    MARQUEZ_A("Marquez, Alex", R.drawable.alex_marquez_gresini_racing_1),
+    RINS("Rins, Alex", R.drawable.alex_rins_lcr_honda_1),
+    FERNANDEZ_A("Fernandez, Augusto", R.drawable.augusto_fernandez_gasgas_facto_1),
+    BINDER("Binder, Brad", R.drawable.brad_binder_red_bull_ktm_facto_1),
+    BASTIANINI("Bastianini, Enea", R.drawable.enea_bastianini_ducati_team_1),
+    QUARTARARO("Quartararo, Fabio", R.drawable.fabio_quartararo_yamaha_factor_1),
+    GIANNATONIO("Di Giannantonio, Fabio", R.drawable.fabio_di_giannantonio_gresini__1),
+    BAGNAIA("Bagnaia, Francesco", R.drawable.francesco_bagnaia_ducati_team_1),
+    MORDBIDELLI("Morbidelli, Franco", R.drawable.franco_morbidelli_yamaha_facto_1),
+    MILLER("Miller, Jack", R.drawable.jack_miller_red_bull_ktm_facto_1),
+    MIR("Mir, Joan", R.drawable.joan_mir_repsol_honda_team_1),
+    MARINI("Marini, Luca", R.drawable.luca_marini_vr46_racing_team_1),
+    MARQUEZ_M("Marquez, Marc", R.drawable.marc_marquez_repsol_honda_team_1),
+    BEZZECCHI("Bezzecchi, Marco", R.drawable.marco_bezzecchi_vr46_racing_te_1),
+    VINIALES("Vinales, Maverick", R.drawable.maverick_vinales_aprilia_racin_1),
+    OLIVEIRA("Oliveira, Miguel", R.drawable.miguel_oliveira_rnf_racing_1),
+    ESPARGARO_P("Espargaro, Pol", R.drawable.pol_espargaro_gasgas_factory_r_1),
+    FERNANDEZ_R("Fernandez, Raul", R.drawable.raul_fernandez_rnf_racing_1),
+    NAKAGAMI("Nakagami, Takaaki", R.drawable.takaaki_nakagami_lcr_honda_1),
+
+}
